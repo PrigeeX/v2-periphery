ef94f0ccb697278dd0561e66e6bca06c1f85eb0b
Jay-Sojitra <jaysojitra1011@gmail.com>
2026-05-21 12:45:41 +0530

    chore: update init code hash for PrigeeX local build

--- contracts/libraries/UniswapV2Library.sol
+++ contracts/libraries/UniswapV2Library.sol
@@ -21,7 +21,7 @@ library UniswapV2Library {
                 hex'ff',
                 factory,
                 keccak256(abi.encodePacked(token0, token1)),
-                hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
+                hex'4398b8f476c0f6a9e51b3219630fc60a8d26f67408329f8d4d57226ad638d75d' // init code hash (PrigeeX local build)
             ))));
     }
 
